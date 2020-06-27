63d3ebcca7ad5c30a7ad5cd1c49598f5b102259f
marceloito <63684519+marceloito@users.noreply.github.com>
2020-06-27 00:20:50 -0300

    Add files via upload

--- srcUNIVALIPS/main/java/com/example/univaliprestacaoservico/MainActivity.java
+++ srcUNIVALIPS/main/java/com/example/univaliprestacaoservico/MainActivity.java
@@ -1,27 +1,42 @@
 package com.example.univaliprestacaoservico;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    SQLiteDatabase bancoDeDados;
+    SQLiteDatabase bancoDeDados; // Para lembrar de inserir o banco de dados depois porque não manjo de B.D. haha :( .
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /*Este botão é para acessar a intent de Prestação de Serviço.*/
         Button botaoPS = findViewById(R.id.botaoPS);
         botaoPS.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(getApplicationContext(), PrestServicoActivity.class);
                 startActivity(i);
+            }
+        });
+
+        /* Apenas inseri este botão no aplicativo para seguir as instruções do trabalho.
+        *  Também não fiz a implementação dos outros botões porque não ví muita necessidade
+        *  no momento, se sobrar um tempo a gente usa esses botões para alguma outra função...
+        *  O que esse botão faz:
+        *  Ele imprimi um mensagem do tipo Toast indicando que o botão é só para teste.*/
+        Button botaoServico01 = findViewById(R.id.botaoServico01);
+        botaoServico01.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), "Este botão é um teste...", Toast.LENGTH_SHORT);
+                toast.show();
             }
         });
     }
--- srcUNIVALIPS/main/java/com/example/univaliprestacaoservico/PrestServicoActivity.java
+++ srcUNIVALIPS/main/java/com/example/univaliprestacaoservico/PrestServicoActivity.java
@@ -2,22 +2,27 @@ package com.example.univaliprestacaoservico;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class PrestServicoActivity extends AppCompatActivity {
-
+    Intent intent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prest_servico);
-    }
+        intent = getIntent();
 
-    void openDialog () {
-        Context context = getApplicationContext();
-        CharSequence text = "Este Botão é um teste...";
-        int duration = Toast.LENGTH_SHORT;
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
+        Button botaoVoltar = findViewById(R.id.botaoVoltar);
+        botaoVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
+
